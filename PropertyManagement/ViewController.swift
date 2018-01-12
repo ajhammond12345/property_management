ece8480028bff3c817062c026f333507cea393e8
Alexander Hammond <ajhammond123@gmail.com>
2018-01-11 19:59:09 -0500

    Initial Commit

---
+++ PropertyManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropertyManagement
+//
+//  Created by Alexander Hammond on 1/11/18.
+//  Copyright © 2018 RAA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
